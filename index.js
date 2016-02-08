7b5e419191b006e2fe7b7988a64b5e07ff016505
Patt-tom McDonnell <contact@mcdonnell.pt>
2016-02-08 11:07:06 +0000

    Remove logging

--- index.js
+++ index.js
@@ -12,21 +12,17 @@ module.exports = function(options){
 	var defaultPayload = options.payload  || defaultPayload;
 
 	function sendRequest(payload, cb){
-		console.log('send', payload);
 		return request.post(webhookUrl).send(payload).end(cb)
 	}
 
 	return {
 		send: function(text){
-			console.log('send');
 			var payload = {
 				text: text
 			}
 			var deferred = q.defer();
 			sendRequest(_.merge(defaultPayload, payload), function(err, res){
-				console.log(res);
 				if(err){
-					console.log('err');
 					deferred.reject(err)
 				}
 				deferred.resolve(res)
